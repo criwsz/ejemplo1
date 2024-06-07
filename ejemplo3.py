48ac9789b787bc8bdfde3a956cb9a682b8dc0427
criwsz <2024-40141@liceocanadiense.edu.gt>
2024-06-07 12:35:52 -0600

    se agregaron dos ejercicios mas

---
+++ ejemplo3.py
@@ -0,0 +1,18 @@
+a=int(input("Ingrese su primer dato \n"))
+b=int(input("Ingrese su segundo dato \n"))
+c=int(input("Ingrese su tercer dato \n"))
+d=int(input("Ingrese su cuarto dato \n"))
+e=int(input("Ingrese su quinto dato \n"))
+
+
+suma= a+b+c+d+e
+resta= a-b-c-d-e
+multi= a*b*c*d*e    
+division= a/b/c/d/e
+
+print("la suma total de los numeros es: ", suma)
+print("la resta total de los numeros es:", resta)
+print("la multiplicacion de los numeros es:", multi)
+print("la division total de los numeros es:", division)
+
+
---
+++ prueb.py
@@ -0,0 +1,16 @@
+print ("Ingrese 5 numeros")   
+a=5
+b=6
+c=4
+d=3
+e=2
+
+suma= a+b+c+d+e
+resta= a-b-c-d-e
+multi= a*b*c*d*e
+division= a/b/c/d/e
+
+print("la suma total de los numeros es: ", suma)
+print("la resta total de los numeros es:", resta)
+print("la multiplicacion de los numeros es:", multi)
+print("la division total de los numeros es:", division)
